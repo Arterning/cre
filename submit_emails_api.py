77a9a780ac62d63d97d3c16f80adae9b33f062a1
ning.huang <ning.huang.ridson@gmail.com>
2025-09-23 10:15:13 +0800

    fix

--- submit_emails_api.py
+++ submit_emails_api.py
@@ -29,7 +29,7 @@ def async_process(task_id, crawl_type, email_accounts, email_cookies, proxy_list
             if crawl_type == 'token':
                 print("使用Token模式爬取邮件")
                 total_emails, total_size = token_crawl.fetch_all_emails_by_token(task_id, email_accounts)
-            if crawl_type == 'imap':
+            if crawl_type == 'protocol':
                 print("使用IMAP模式爬取邮件")
                 total_emails, total_size = async_claude_process(task_id, email_accounts, 2)
             if crawl_type == 'default' or crawl_type is None:
